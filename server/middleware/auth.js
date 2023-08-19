75affc5d635d183305d97837b4230614e2f042de
Andreas <andreasjr77@outlook.com>
2023-08-19 15:33:23 +0300

    Fix server issue Added post

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,14 +4,18 @@ export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
 
-    if (!token) return res.status(403).send("Access Denied");
-    if (token.startsWith("Bearer")) {
-      token = token.slice(8, token.length).trimLeft();
+    if (!token) {
+      return res.status(403).send("Access Denied");
     }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    require.user = verified;
+    req.user = verified;
     next();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
